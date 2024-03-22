7185568aa7347671e2a0513186e0b9b3346e1328
le tu <lethucthanhtu@gmail.com>
2024-03-22 13:45:22 +0700

    refactor: add gradle & src folder

---
+++ src/main/java/com/theeduconnect/exeeduconnectbe/ExeEduConnectBeApplication.java
@@ -0,0 +1,13 @@
+package com.theeduconnect.exeeduconnectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeEduConnectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeEduConnectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theeduconnect/exeeduconnectbe/ExeEduConnectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theeduconnect.exeeduconnectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeEduConnectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
