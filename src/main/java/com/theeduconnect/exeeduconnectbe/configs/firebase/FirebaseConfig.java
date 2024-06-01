bc7b5ac605b544f1b022ab4cbb5ec87573e8a224
dominhhieuk2 <hokkaidod8@gmail.com>
2024-06-01 17:27:47 +0700

    modify path to file firebaseConfig.json

--- src/main/java/com/theeduconnect/exeeduconnectbe/configs/firebase/FirebaseConfig.java
+++ src/main/java/com/theeduconnect/exeeduconnectbe/configs/firebase/FirebaseConfig.java
@@ -3,26 +3,27 @@ package com.theeduconnect.exeeduconnectbe.configs.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream(
-                        "src/main/resources/educonnectdb-firebase-adminsdk-vcnpg-39111bcd74.json");
+        ClassPathResource serviceAccountResource = new ClassPathResource("firebaseConfig.json");
 
-        FirebaseOptions options =
-                new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                        .setStorageBucket("educonnectdb.appspot.com")
-                        .build();
+        try (InputStream serviceAccount = serviceAccountResource.getInputStream()) {
+            FirebaseOptions options =
+                    new FirebaseOptions.Builder()
+                            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                            .setStorageBucket("educonnectdb.appspot.com")
+                            .build();
 
-        FirebaseApp.initializeApp(options);
+            FirebaseApp.initializeApp(options);
+        }
     }
 }
